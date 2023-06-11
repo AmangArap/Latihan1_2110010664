b72869436ab49dd219ea77a371c050c8ee0d4f19
AmangArap <raftlee123@gmail.com>
2023-06-12 01:22:39 +0800

    Membuat class MatematikaBeraksi

--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -10,5 +10,15 @@ package Hitung;
  * @author asus
  */
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+      //Membuat objek
+      Matematika rafli = new Matematika(6,4);
+      
+        System.out.println("Hasil Penjumlahan : " + rafli.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + rafli.setPengurangan());
+        System.out.println("Hasil Perkalian : " + rafli.setPerkalian());
+        System.out.println("Hasil Pembagian : " + rafli.setPembagian());
+
+    }
     
 }
